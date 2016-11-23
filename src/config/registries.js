3f0e045cacefcc826dc93faf8cd3fbe4b8779a7d
Freax <huangyanxiong2013@gmail.com>
2016-11-24 00:30:33 +0800

    rename

---
+++ src/config/registries.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Freax on 16-11-22.
+ * @Blog http://www.myfreax.com/
+ */
+export default  [
+    {
+        name: 'npm',
+        home: 'https://www.npmjs.org',
+        registry: 'https://registry.npmjs.org'
+    },
+    {
+        name: 'cnpm',
+        home: 'http://cnpmjs.org',
+        registry: 'http://r.cnpmjs.org'
+    },
+    {
+        name: 'taobao',
+        home: 'https://npm.taobao.org',
+        registry: 'https://registry.npm.taobao.org'
+    },
+    {
+        name: 'nj',
+        home: 'https://www.nodejitsu.com',
+        registry: 'https://registry.nodejitsu.com'
+    },
+    {
+        name: 'rednpm',
+        home: 'http://npm.mirror.cqupt.edu.cn/',
+        registry: 'http://registry.mirror.cqupt.edu.cn'
+    },
+    {
+        name: 'npmMirror',
+        home: 'https://skimdb.npmjs.com/',
+        registry: 'https://skimdb.npmjs.com/registry'
+    },
+    {
+        name: 'edunpm',
+        home: 'http://www.enpmjs.org',
+        registry: 'http://registry.enpmjs.org'
+    }
+]
