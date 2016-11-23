82e49830bcad4ffd2c06c9bfff5b250c4ee07cb5
Freax <huangyanxiong2013@gmail.com>
2016-11-23 13:36:28 +0800

    add run command function into commmon

--- src/common.js
+++ src/common.js
@@ -4,18 +4,37 @@
  */
 import * as keys from './config/keys'
 import Grid from 'term-grid'
-import terminalConfig from './config/terminal'
+import {exec} from 'child_process'
 
 
-
-export let drawGrid  = registries => {
-   let registryArray =  (registries || []).map((value) => {
+export let drawGrid = registries => {
+    const {NAME, HOME, REGISTRY} = keys;
+    let registryArray = (registries || []).map((value) => {
         let temp = [];
         Object.keys(keys).forEach((key) => {
             temp.push(value[keys[key]] || '');
         });
         return temp;
     });
-    new Grid(terminalConfig.concat(registryArray)).draw();
+    new Grid([[NAME, REGISTRY, HOME]].concat(registryArray)).draw();
+    return 0;
 };
 
+export let run = (command) => {
+   return new Promise((resolve, reject) => {
+        exec(command, (error, stdout) => {
+            if (error) {
+                reject(error);
+                return false;
+            }
+            resolve(stdout);
+            return true;
+        });
+    }).catch((err) => {
+        console.log(err.stack);
+    });
+};
+
+export let filterRegister = ()=>{
+
+};
