14ca9904c13e9c0483087e382fda4a569679c969
Freax <huangyanxiong2013@gmail.com>
2016-11-24 22:58:03 +0800

    separation action

--- src/index.js
+++ src/index.js
@@ -5,117 +5,37 @@
  * @Blog http://www.myfreax.com/
  */
 
-import registries from './config/registries'
 import program from 'commander'
-import {drawGrid, run, registerFilter, writeFilePromise, trim} from './common'
-import {NAME, HOME, REGISTRY} from './config/keys'
-import toSource from 'tosource'
-
+import {ls,current,add,use,del} from './actions'
 
 program
     .version('0.0.1');
 
-
 program
     .command('current')
     .description('Show current registry name')
-    .action(async() => {
-
-        let currentRegistry = await run('yarn config get registry');
-
-        currentRegistry = trim(currentRegistry);
-
-        if (currentRegistry === 'undefined' || !currentRegistry) {
-            drawGrid();
-        }
-
-        let currentRegistries = registerFilter(registries, registry => {
-            return trim(registry[REGISTRY]) === currentRegistry;
-        });
-
-        drawGrid(currentRegistries);
-
-        return true;
-    });
+    .action(current);
 
 program
     .command('ls')
     .description('List all the registries')
-    .action(() => {
-        drawGrid(registries);
-    });
+    .action(ls);
 
 
 program
     .command('use <registryName>')
     .description('Change registry to registry')
-    .action(async(env, options) => {
-
-        let registry = registerFilter(registries, registry => {
-            return trim(registry.name) === trim(env);
-        });
-
-        if (registry.length === 0) {
-            console.info('--help');
-            return true;
-        }
-
-        await run(`yarn config set registry  ${registry[0][REGISTRY]}`);
-
-        return true;
-    });
+    .action(use);
 
 program
-    .command('add <registry> [registryName]')
+    .command('add <registryName> <url> [home]')
     .description('Add one custom registry')
-    .action(async(env, options) => {
-
-        let registry = registerFilter(registries, registry => {
-            return trim(registry.name) === trim(env);
-        });
-
-        options = trim(options);
-
-        if (!/^http|https:\/\/[a-z]+\.[a-z\d]+\.[a-z]+$/i.test(options)) {
-            console.info(`Incorrect registry ${options}`);
-            return true;
-        }
-
-        if (registry.length !== 0) {
-            console.info(`The registryName ${env} already exists`);
-            return true;
-        }
-
-        let temp = {};
-        temp[NAME] = env;
-        temp[REGISTRY] = options;
-        registries.push(temp);
-
-        await writeFilePromise(toSource(registries));
-
-        return true;
-    });
+    .action(add);
 
 program
     .command('del <registryName>')
     .description('Delete one custom registry')
-    .action(async(env) => {
-
-        let registry = registerFilter(registries, registry => {
-            return trim(registry.name) === trim(env);
-        });
-
-        if (registry.length === 0) {
-            console.info(`The registryName ${env} is not exists`);
-            return true;
-        }
-
-        registries.splice(registries.indexOf(registry[0], 1));
-
-        await writeFilePromise(toSource(registries));
-
-        return true;
-    });
+    .action(del);
 
 
 program.parse(process.argv);
