0cc42d068411847c70fb568dbff239b74fb8bd62
Freax <huangyanxiong2013@gmail.com>
2016-11-24 22:44:16 +0800

    separation of api

---
+++ src/config/api.js
@@ -0,0 +1,9 @@
+// @flow
+/**
+ * Created by Freax on 16-11-24.
+ * @Blog http://www.myfreax.com/
+ */
+export default {
+    current:'yarn config get registry',
+    use:'yarn config set registry'
+}
